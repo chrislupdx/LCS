60ea69422cb3a67bdb0df87377432a341ade3619
chrislupdx <christopherlu.pdx@gmail.com>
2021-11-16 18:42:45 -0800

    first commit

---
+++ LCS.js
@@ -0,0 +1,24 @@
+//This is hopefully two JS implementations of LCS (memoized vs brute force)
+
+//this is the tabulated implemenation of LCS
+function tLCS()
+{
+    console.log("tLCS");
+}
+
+//this is the recursive implementation of LCS
+function rLCS()
+{
+    console.log("rLCS");
+}
+
+
+//this is the interface funciton
+function test()
+{
+    console.log("fuckyou");
+
+    //generate new stuff
+}
+
+test();
